97d18b59c8038cc8a917b98612e72b65584a1c1d
Nidhzz07 <100823602+Nidhzz07@users.noreply.github.com>
2022-04-19 12:22:56 +0530

    Add files via upload

---
+++ p1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNetPractical_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, sum = 0, m;
+            Console.Write("Enter a number: ");
+            n = int.Parse(Console.ReadLine());
+            while (n > 0)
+            {
+                m = n % 10;
+                sum = sum + m;
+                n = n / 10;
+            }
+            Console.Write("Sum is= " + sum);
+        }
+    }
+}
